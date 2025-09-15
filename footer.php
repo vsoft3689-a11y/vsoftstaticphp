41cd03d2541721276f14538de2ea0b586a4819df
ThakurBhagathSingh <bhagathsinghthakur7391@gmail.com>
2025-09-15 17:40:49 +0530

    index

--- footer.php
+++ footer.php
@@ -45,8 +45,8 @@
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6">
-                    <h4 class="text-white mb-3">Newsletter</h4>
-                    <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+                    <h4 class="text-white mb-3">Subscribe to our Newsletter for Updates</h4>
+                    <p>Stay connected! Get course updates and career tips delivered to your email.</p>
                     <div class="position-relative mx-auto" style="max-width: 400px;">
                         <input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
                         <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
@@ -58,7 +58,7 @@
             <div class="copyright">
                 <div class="row">
                     <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        &copy; <a class="border-bottom" href="#">Your Site Name</a>, All Right Reserved.
+                        &copy; <a class="border-bottom" href="#">VsoftSolutions</a>, All Right Reserved.
 
                         <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
                         Designed By <a class="border-bottom" href="./">WesTechnologies.in</a><br><br>
--- index.php
+++ index.php
@@ -55,6 +55,25 @@
   display: inline-block;
   margin: 0;
 } */
+  .price-box {
+  text-align: center;
+  margin: 10px 0;
+  font-family: Arial, sans-serif; /* Clean font */
+}
+
+.old-price {
+  font-size: 1.5rem;
+  color: #888;
+  text-decoration: line-through;
+  margin-right: 10px;
+}
+
+.new-price {
+  font-size: 2rem;
+  font-weight: bold;
+  color: #e63946; /* Red for discount */
+}
+
 
     </style>
     
@@ -247,7 +266,7 @@
                     <div class="row g-3">
                         <div class="col-lg-12 col-md-12 wow zoomIn" data-wow-delay="0.1s">
                             <a class="position-relative d-block overflow-hidden" href="">
-                                <img class="img-fluid" src="img/sathArtifial.jpg" alt="">
+                                <img class="img-fluid" src="img/ai.jpg" alt="">
                                 <div class="bg-white text-center position-absolute bottom-0 end-0 py-2 px-3" style="margin: 1px;">
                                     <h5 class="m-0">Artificial Intelligence</h5>
                                     <!-- <small class="text-primary">49 Courses</small> -->
@@ -276,7 +295,7 @@
                 </div>
                 <div class="col-lg-5 col-md-6 wow zoomIn" data-wow-delay="0.7s" style="min-height: 350px;">
                     <a class="position-relative d-block h-100 overflow-hidden" href="">
-                        <img class="img-fluid position-absolute w-100 h-100" src="img/sathvikamarketing.jpg" alt="" style="object-fit: cover;">
+                        <img class="img-fluid position-absolute w-100 h-100" src="img/marketing.jpg" alt="" style="object-fit: cover;">
                         <div class="bg-white text-center position-absolute bottom-0 end-0 py-2 px-3" style="margin:  1px;">
                             <h5 class="m-0">Marketing</h5>
                             <!-- <small class="text-primary">49 Courses</small> -->
@@ -290,10 +309,60 @@
 
 
     <div class="container-xxl py-5">
-         <div class="container"> 
-            <div class="text-center wow fadeInUp" data-wow-delay="0.1s"> 
-                <h6 class="section-title bg-white text-center text-primary px-3">Courses</h6> 
-                <h1 class="mb-5">Popular Courses</h1> </div> <div class="row g-4 justify-content-center"> <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s"> <div class="course-item bg-light"> <div class="position-relative overflow-hidden"> <img class="img-fluid" src="img/mini.jpg" alt=""> <div class="w-100 d-flex justify-content-center position-absolute bottom-0 start-0 mb-4"> <!-- <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3 border-end" style="border-radius: 30px 0 0 30px;">Read More</a> <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3" style="border-radius: 0 30px 30px 0;">Join Now</a> --> </div> </div> <div class="text-center p-4 pb-0"> <h3 class="mb-0">₹13999.00</h3> <div class="mb-3"> <!-- <small class="fa fa-star text-primary"></small> <small class="fa fa-star text-primary"></small> <small class="fa fa-star text-primary"></small> <small class="fa fa-star text-primary"></small> <small class="fa fa-star text-primary"></small> --> <!-- <small>(123)</small> --> </div> <h5 class="mb-4">Mini Projects</h5> </div> <!-- <div class="d-flex border-top"> <small class="flex-fill text-center border-end py-2"><i class="fa fa-user-tie text-primary me-2"></i>John Doe</small> <small class="flex-fill text-center border-end py-2"><i class="fa fa-clock text-primary me-2"></i>1.49 Hrs</small> <small class="flex-fill text-center py-2"><i class="fa fa-user text-primary me-2"></i>30 Students</small> </div> --> </div> </div> <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s"> <div class="course-item bg-light"> <div class="position-relative overflow-hidden"> <img class="img-fluid" src="img/major.jpg" alt=""> <div class="w-100 d-flex justify-content-center position-absolute bottom-0 start-0 mb-4"> <!-- <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3 border-end" style="border-radius: 30px 0 0 30px;">Read More</a> <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3" style="border-radius: 0 30px 30px 0;">Join Now</a> --> </div> </div> <div class="text-center p-4 pb-0"> <h3 class="mb-0">₹16999.00</h3> <div class="mb-3"> <!-- <small class="fa fa-star text-primary"></small> <small class="fa fa-star text-primary"></small> <small class="fa fa-star text-primary"></small> <small class="fa fa-star text-primary"></small> <small class="fa fa-star text-primary"></small> <small>(123)</small> --> </div> <h5 class="mb-4">Major Projects</h5> </div> <!-- <div class="d-flex border-top"> <small class="flex-fill text-center border-end py-2"><i class="fa fa-user-tie text-primary me-2"></i>John Doe</small> <small class="flex-fill text-center border-end py-2"><i class="fa fa-clock text-primary me-2"></i>1.49 Hrs</small> <small class="flex-fill text-center py-2"><i class="fa fa-user text-primary me-2"></i>30 Students</small> </div> --> </div> </div> <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s"> <div class="course-item bg-light"> <div class="position-relative overflow-hidden"> <img class="img-fluid" src="img/sathInternshp.jpg" alt=""> <div class="w-100 d-flex justify-content-center position-absolute bottom-0 start-0 mb-4"> <!-- <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3 border-end" style="border-radius: 30px 0 0 30px;">Read More</a> <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3" style="border-radius: 0 30px 30px 0;">Join Now</a> --> </div> </div> <div class="text-center p-4 pb-0"> <h3 class="mb-0">₹17999.00</h3> <div class="mb-3"> <!-- <small class="fa fa-star text-primary"></small> <small class="fa fa-star text-primary"></small> <small class="fa fa-star text-primary"></small> <small class="fa fa-star text-primary"></small> <small class="fa fa-star text-primary"></small> <small>(123)</small> --> </div> <h5 class="mb-4">Internship & Corporate</h5> </div> <!-- <div class="d-flex border-top"> <small class="flex-fill text-center border-end py-2"><i class="fa fa-user-tie text-primary me-2"></i>John Doe</small> <small class="flex-fill text-center border-end py-2"><i class="fa fa-clock text-primary me-2"></i>1.49 Hrs</small> <small class="flex-fill text-center py-2"><i class="fa fa-user text-primary me-2"></i>30 Students</small> </div> --> </div> </div> </div> </div> </div>
+  <div class="container">
+    <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+      <h6 class="section-title bg-white text-center text-primary px-3">Courses</h6>
+      <h1 class="mb-5">Popular Courses</h1>
+    </div>
+
+    <!-- Row for 3 cards -->
+    <div class="row g-4">
+      
+      <!-- Mini Projects -->
+      <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+        <div class="course-item bg-light">
+          <div class="position-relative overflow-hidden">
+            <img class="img-fluid" src="img/mini.jpg" alt="">
+          </div>
+          <div class="price-box text-center mt-3">
+            <span class="old-price">₹1999.00</span>
+            <span class="new-price">₹1599.00</span>
+          </div>
+          <h5 class="text-center mb-4">Mini Projects</h5>
+        </div>
+      </div>
+
+      <!-- Major Projects -->
+      <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+        <div class="course-item bg-light">
+          <div class="position-relative overflow-hidden">
+            <img class="img-fluid" src="img/major.jpg" alt="">
+          </div>
+          <div class="price-box text-center mt-3">
+            <span class="old-price">₹1999.00</span>
+            <span class="new-price">₹1599.00</span>
+          </div>
+          <h5 class="text-center mb-4">Major Projects</h5>
+        </div>
+      </div>
+
+      <!-- Internship & Corporate -->
+      <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+        <div class="course-item bg-light">
+          <div class="position-relative overflow-hidden">
+            <img class="img-fluid" src="img/sathInternshp.jpg" alt="">
+          </div>
+          <div class="price-box text-center mt-3">
+            <span class="old-price">₹1999.00</span>
+            <span class="new-price">₹1599.00</span>
+          </div>
+          <h5 class="text-center mb-4">Internship & Corporate</h5>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
 
 
     
@@ -301,7 +370,7 @@
 
 
     <!-- Team Start -->
-    <div class="container-xxl py-5">
+    <!-- <div class="container-xxl py-5">
         <div class="container">
             <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
                 <h6 class="section-title bg-white text-center text-primary px-3">Instructors</h6>
@@ -382,7 +451,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
     <!-- Team End -->
 
 
