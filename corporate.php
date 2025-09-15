46ecebfc43c96033f2dba08c11c78f893241606a
beeramthirumal <beeramthirumalreddy@gmail.com>
2025-09-15 19:41:31 +0530

    internship nav

--- corporate.php
+++ corporate.php
@@ -43,42 +43,7 @@
 
 
     <!-- Navbar Start -->
-<<<<<<< HEAD
       <?php include 'navbar.php'; ?>
-=======
-    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
-        <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
-            <h2 class="m-0 text-primary"><i class="fa fa-book me-3"></i>VSoft Solutions</h2>
-        </a>
-        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="./index.php" class="nav-item nav-link active">Home</a>
-                <a href="./about.php" class="nav-item nav-link">About</a>
-                <a href="./courses.php" class="nav-item nav-link">Services</a>
-                <a href="./projects.php" class="nav-item nav-link">Projects</a>
-                 <!-- <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Projects</a>
-                    <div class="dropdown-menu fade-down m-0"> -->
-
-                <!-- <a href="team.html" class="dropdown-item">BTECH Projects</a>
-                        <a href="testimonial.html" class="dropdown-item">MTECH Projects</a>
-                        <a href="404.html" class="dropdown-item">MBA Projects</a>
-                        <a href="team.html" class="dropdown-item">MCA Projects</a> -->
-                        <!-- <a href="testimonial.html" class="dropdown-item">Internship & Corporate</a> -->
-                        
-                    <!-- </div>
-                </div> -->
-                <a href="./internship.php" class="nav-item nav-link">Internship</a>
-                <!-- <a href="./courses.php" class="nav-item nav-link">Internship & Corporate</a> -->
-                <a href="./contact.php" class="nav-item nav-link">Contact</a>
-            </div>
-            <a href="" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Join Now<i class="fa fa-arrow-right ms-3"></i></a>
-        </div>
-    </nav>
->>>>>>> 9a0ed7182c1d565ad0d34c1c5b502dcb10200c7f
     <!-- Navbar End -->
 
 
